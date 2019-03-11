68b6bf66e39ab8e2b8f65da54d1ff1001f6f8fe5
藤田優作 <fujitayuusaku@fujitayuusakunoMacBook-Pro.local>
2019-03-11 23:14:06 +0900

    プロジェクトファイルを作成した
    swiftの練習のため

---
+++ SwiftButton1/NextViewController.swift
@@ -0,0 +1,34 @@
+//
+//  NextViewController.swift
+//  SwiftButton1
+//
+//  Created by 藤田優作 on 2019/03/08.
+//  Copyright © 2019 藤田優作. All rights reserved.
+//
+
+import UIKit
+
+class NextViewController: UIViewController {
+
+    @IBOutlet var changeLabel: UILabel!
+    
+    
+    @IBAction func change(_ sender: Any) {
+        
+        //ここが呼ばれる
+        //次の画面へと書かれたラベルの文字を変更したい
+        
+        changeLabel.text = "暗号が解除されました。"
+        
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
--- SwiftButton1/ViewController.swift
+++ SwiftButton1/ViewController.swift
@@ -9,11 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //countという変数の中に５が入っています。
+    //countという変数を下で使うことができるようになりました。
+    //var count = 5
+  //  var moji="アイウエオ"
+    
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //勝手に呼ばれる箇所
+        
+        
+        
+        
+        
     }
+    
+    @IBAction func tap(_ sender: Any) {
+        
+        
+        
+        //ここに太陽を消すなどのプログラムを書く
+        
+    }
+    //メソッド
+    func aaaaa(){
+        //呼び出してあげないと呼ばれない箇所
+    }
+    
 
 
 }
